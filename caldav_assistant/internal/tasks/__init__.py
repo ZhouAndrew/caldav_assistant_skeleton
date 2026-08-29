a198556c3ef856df16b49f9a6907dffaa42a7b9c
ZhouAndrew <138282146+ZhouAndrew@users.noreply.github.com>
2026-08-29 10:55:45 +0800

    Export CalDAV work lifecycle Task service

--- caldav_assistant/internal/tasks/__init__.py
+++ caldav_assistant/internal/tasks/__init__.py
@@ -1,8 +1,10 @@
 from .service import TaskService
+from .work_service import CalDAVWorkTaskService
 from .completion_log import CompletionLoggingTaskService, TaskCompletionLogService
 
 __all__ = [
     "TaskService",
+    "CalDAVWorkTaskService",
     "TaskCompletionLogService",
     "CompletionLoggingTaskService",
 ]
