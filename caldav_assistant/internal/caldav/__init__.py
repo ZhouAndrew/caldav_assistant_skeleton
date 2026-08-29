c88ca63c4235c8cc83673f2e0a1be4ecdb14c8df
ZhouAndrew <138282146+ZhouAndrew@users.noreply.github.com>
2026-08-29 10:59:34 +0800

    Export collection routing CalDAV adapter

--- caldav_assistant/internal/caldav/__init__.py
+++ caldav_assistant/internal/caldav/__init__.py
@@ -1,11 +1,14 @@
 from .adapter import CalDAVAdapter
-from .library_adapter import LibraryCalDAVAdapter, BaseURLProvider
-from .sync import SyncEngine
-__all__=["CalDAVAdapter","LibraryCalDAVAdapter","BaseURLProvider","SyncEngine"]
-
-# CALDAV_ASSISTANT_PRODUCTION_INTEGRATION_V1
+from .library_adapter import BaseURLProvider, LibraryCalDAVAdapter
+from .routing import CollectionRoutingCalDAVAdapter
 from .setup import CalDAVSetupService
-if '__all__' in globals():
-    __all__ = list(__all__)
-    if 'CalDAVSetupService' not in __all__:
-        __all__.append('CalDAVSetupService')
+from .sync import SyncEngine
+
+__all__ = [
+    "CalDAVAdapter",
+    "LibraryCalDAVAdapter",
+    "CollectionRoutingCalDAVAdapter",
+    "BaseURLProvider",
+    "SyncEngine",
+    "CalDAVSetupService",
+]
