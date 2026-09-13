8d920333906c7bb36246a2c9a4f4875f271c1512
ZhouAndrew <138282146+ZhouAndrew@users.noreply.github.com>
2026-09-13 15:29:43 +0800

    Use post-sync ready snapshot wrapper

--- caldav_assistant/internal/caldav/__init__.py
+++ caldav_assistant/internal/caldav/__init__.py
@@ -3,8 +3,8 @@ from .library_adapter import BaseURLProvider, LibraryCalDAVAdapter
 from .optimized_cache import ExperimentalCacheCalDAVAdapter
 from .optimized_routing import CollectionRoutingCalDAVAdapter
 from .ready_fallback import OfflineFallbackCalDAVAdapter
+from .ready_sync import SyncEngine
 from .setup import CalDAVSetupService
-from .sync import SyncEngine
 
 __all__ = [
     "CalDAVAdapter",
